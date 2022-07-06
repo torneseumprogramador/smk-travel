30c9232ed8d27603dac74297d8db27627f9fd1e7
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-07-06 16:08:46 -0300

    eu controlo nulable

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace smk_travel.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
